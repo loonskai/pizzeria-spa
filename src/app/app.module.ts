6d677020d56c8813967f941f64a6d9aa55150d1e
Siarhei Lunski <the.sergaunt@gmail.com>
2019-05-10 22:38:43 +0300

    Create main-item-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainItemCardComponent } from './main-item-card/main-item-card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MainItemCardComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/main-item-card/main-item-card.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-item-card',
+  templateUrl: './main-item-card.html'
+})
+export class MainItemCardComponent {}
