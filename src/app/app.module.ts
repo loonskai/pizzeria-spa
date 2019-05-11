163b18ec51c82c96fe2a8942cd6d79a502a0d824
Siarhei Lunski <the.sergaunt@gmail.com>
2019-05-11 11:11:13 +0300

    add open card modal button styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ItemCardComponent } from './item-card/item-card.component';
-import { BasketCardComponent } from './basket-card/basket-card.component';
-import { ItemListComponent } from './item-list/item-list.component';
-import { AddButtonComponent } from './add-button/add-button.component';
-import { ItemCardModalComponent } from './item-card-modal/item-card-modal.component';
+import { ItemCardComponent } from './components/item-card/item-card.component';
+import { BasketCardComponent } from './components/basket-card/basket-card.component';
+import { ItemListComponent } from './components/item-list/item-list.component';
+import { AddButtonComponent } from './components/add-button/add-button.component';
+import { ItemCardModalComponent } from './components/item-card-modal/item-card-modal.component';
 
 @NgModule({
-  declarations: [AppComponent, ItemCardComponent, BasketCardComponent, ItemListComponent, AddButtonComponent, ItemCardModalComponent],
+  declarations: [
+    AppComponent,
+    ItemCardComponent,
+    BasketCardComponent,
+    ItemListComponent,
+    AddButtonComponent,
+    ItemCardModalComponent
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
