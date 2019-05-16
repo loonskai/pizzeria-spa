7cefad6e809e6141381ccf34ee8fe12d3556c206
Siarhei Lunski <the.sergaunt@gmail.com>
2019-05-16 10:06:33 +0300

    fix join pipe

--- src/app/pipes/join.pipe.ts
+++ src/app/pipes/join.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({ name: 'join' })
+@Pipe({ name: 'join', pure: false })
 export class JoinPipe implements PipeTransform {
   transform(array: string[]): string {
     return array.join(', ');
