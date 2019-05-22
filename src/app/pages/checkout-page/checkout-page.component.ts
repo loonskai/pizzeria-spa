381f589f89ae34f806368e7883169a25b02bff1a
Siarhei Lunski <the.sergaunt@gmail.com>
2019-05-22 13:04:25 +0300

    implement controller on checkout page

--- src/app/pages/checkout-page/checkout-page.component.ts
+++ src/app/pages/checkout-page/checkout-page.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 
 import { OrderItem } from '../../interfaces';
+import {
+  RemoveFromCart,
+  IncrementCartItemAmount,
+  DecrementCartItemAmount,
+  ClearCart
+} from 'src/app/actions/cart.actions';
 
 @Component({
   selector: 'app-checkout-page',
@@ -17,6 +23,22 @@ export class CheckoutPageComponent {
       .subscribe(cart => {
         this.orderedPizzaItems = cart;
       });
+  }
+
+  removeFromCart(index: number) {
+    this.store.dispatch(new RemoveFromCart(index));
+  }
+
+  incrementAmount(index: number) {
+    this.store.dispatch(new IncrementCartItemAmount(index));
+  }
+
+  decrementAmount(index: number) {
+    this.store.dispatch(new DecrementCartItemAmount(index));
+  }
+
+  clearCart() {
+    this.store.dispatch(new ClearCart());
   }
 
   get totalPrice() {
